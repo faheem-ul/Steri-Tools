ef8b19a99cd49d6dd1cd4fc22b461a85edcbf165
alihassamofficial <hassamali.official@gmail.com>
2025-09-11 17:09:01 +0500

    feat( Our Partners ) Added Circular Rings to UI

--- src/components/OurPartners/Validation/index.tsx
+++ src/components/OurPartners/Validation/index.tsx
@@ -8,6 +8,8 @@ import Button from "@/ui/Button";
 
 import testlabs from "@/public/images/partners/testlabs.svg";
 import northsky from "@/public/images/partners/northsky.svg";
+import yellowDoubleRing from "@/public/images/partners/single-yellow-double-ring.svg";
+import blueYellowRing from "@/public/images/partners/single-yellow-blue-ring.svg";
 
 const Validation = () => {
   const [showContent, setShowContent] = useState(false);
@@ -18,7 +20,18 @@ const Validation = () => {
       </Text>
 
       {/* test labs */}
-      <div className="w-full bg-[#14205A] rounded-[24px] mb-[73px] min-h-[260px] flex md:px-0 px-5">
+      <div className="relative w-full bg-[#14205A] rounded-[24px] mb-[73px] min-h-[260px] flex md:px-0 px-5">
+        {/* Yellow Rings BG */}
+        <Image
+          src={yellowDoubleRing}
+          alt="Rings Background"
+          className="
+           pointer-events-none object-contain
+           mx-auto max-w-[60px] pt-[20px]            /* mobile: center */
+           md:absolute md:left-[29px] md:top-[20px] md:max-w-[75.96px]  /* desktop: positioned */
+          "
+        />
+
         <div className="w-full max-w-[987px] mx-auto md:flex-row flex-col flex justify-between items-center">
           <div className="flex gap-[49px] md:flex-row flex-col items-center py-[30px]">
             <Image src={testlabs} alt="eee" />
@@ -37,6 +50,17 @@ const Validation = () => {
             </Button>
           </div>
         </div>
+
+        {/* BlueYellow Rings BG */}
+        <Image
+          src={blueYellowRing}
+          alt="Rings Background"
+          className="
+          pointer-events-none object-contain
+          mx-auto max-w-[60px] pb-[22px]                   /* mobile: centered */
+          md:absolute md:left-[38.37px] md:bottom-[22px] md:max-w-[56.56px]  /* desktop: positioned */
+          "
+        />
       </div>
 
       <Text className="mb-[73px] text-center font-medium font-montserrat text-[22px]">
@@ -54,7 +78,18 @@ const Validation = () => {
 
       {/* northsky */}
 
-      <div className="w-full bg-[#14205A] rounded-[24px] mb-[73px] min-h-[260px] flex">
+      <div className="relative w-full bg-[#14205A] rounded-[24px] mb-[73px] min-h-[260px] flex">
+        {/* Yellow Rings BG */}
+        <Image
+          src={yellowDoubleRing}
+          alt="Rings Background"
+          className="
+           pointer-events-none object-contain
+           mx-auto max-w-[60px] pt-[20px]            /* mobile: center */
+           md:absolute md:left-[29px] md:top-[20px] md:max-w-[75.96px]  /* desktop: positioned */
+          "
+        />
+
         <div className="w-full max-w-[987px] mx-auto md:flex-row flex-col flex justify-between items-center">
           <div className="flex gap-[49px] items-center py-[30px] md:flex-row flex-col">
             <Image src={northsky} alt="northsky" />
@@ -72,6 +107,17 @@ const Validation = () => {
             </Button>
           </div>
         </div>
+
+        {/* BlueYellow Rings BG */}
+        <Image
+          src={blueYellowRing}
+          alt="Rings Background"
+          className="
+          pointer-events-none object-contain
+          mx-auto max-w-[60px] pb-[22px]                   /* mobile: centered */
+          md:absolute md:left-[38.37px] md:bottom-[22px] md:max-w-[56.56px]  /* desktop: positioned */
+          "
+        />
       </div>
 
       <Text className="mb-[73px] text-center font-medium font-montserrat text-[22px]">
